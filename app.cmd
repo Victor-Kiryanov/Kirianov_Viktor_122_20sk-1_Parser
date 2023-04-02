25d3ea2680cec522903f2fd7f31666b30d83c30d
Victor-Kiryanov <kiryanova063@gmail.com>
2023-04-02 12:19:27 +0300

    Laba Parser Kirianov and jar file with cmd exe

---
+++ app.cmd
@@ -0,0 +1,1 @@
+java -jar parserKirianov-0.0.1-SNAPSHOT.jar 
---
+++ parserKirianov/src/test/java/com/example/parserKirianov/ParserKirianovApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parserKirianov;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParserKirianovApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
